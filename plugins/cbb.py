f886d87c9b4070a7b02bde728e6218311af9158e
Kyuraxp <96605016+Kyuraxp@users.noreply.github.com>
2021-12-28 22:14:29 +0700

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -13,10 +13,10 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text=f"<b>Tentang Bot ini:\n\n • Owner: @{OWNER}\n • Channel: @{CHANNEL}\n • Group: @{GROUP}\n • Source Code: <a href='https://github.com/mrismanaziz/File-Sharing-Man'>Klik Disini</a></b>\n",
+            text=f"<b>Tentang Bot ini:\n\n • Owner: @{OWNER}\n • Channel: @{CHANNEL}\n • Group: @{GROUP}\n • Source Code: <a href='https://github.com/Kyuraxp/File-Sharing-Kyura'>Klik Disini</a></b>\n",
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
-                [[InlineKeyboardButton("• ᴛᴜᴛᴜᴘ •", callback_data="close")]]
+                [[InlineKeyboardButton("• TUTUP KNTL •", callback_data="close")]]
             ),
         )
     elif data == "close":
